fefe238a9e4f5f0a143bd467f7ed61c773f5e5c9
chrisberkner <chrisberkner@users.noreply.github.com>
2020-06-23 12:55:40 -0700

    switch .pkl from Dropbox to Google

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/0skqq5a5odp2byh/export2.pkl?dl=0'
+export_file_url = 'https://drive.google.com/file/d/1n9x5UCz2OjMYXlvuymLIl7kwxdHM-14Y/view?usp=sharing'
 export_file_name = 'export2.pkl'
 
 classes = ['blue_jay', 'green_jay', 'stellars_jay']
